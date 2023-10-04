c4abb2be1c9fc38d90fc135ba445e0c6ab67e67a
Parot Ketpong <parot@mtrgroup.local>
2023-10-04 15:54:42 +0700

    Fixed : Excercise Loop

--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,27 @@
+// Start coding here
 // Exercise #4: Reverse Strings
 let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+let a = companyName.slice(0,1)
+let b = companyName.slice(1,2)
+let c = companyName.slice(2,3)
+let d = companyName.slice(3,4)
+let e = companyName.slice(4,5)
+let f = companyName.slice(5,6)
+let g = companyName.slice(6,7)
+let h = companyName.slice(7,8)
+let i = companyName.slice(8,9)
+let j = companyName.slice(9,10)
+let k = companyName.slice(10,11)
+let l = companyName.slice(11,12)
+let m = companyName.slice(12,13)
+let n = companyName.slice(13,14)
+let o = companyName.slice(14,15)
+reversedCompanyName = o+n+m+l+k+j+i+h+" "+f+e+d+c+b+a
+console.log(reversedCompanyName)
+//for (let i= companyName.length -1 ; i >= 0; i--){
+ //   reversedCompanyName +=companyName[i]
+//}
+//console.log(reversedCompanyName)
--- ex-9.js
+++ ex-9.js
@@ -10,3 +10,34 @@ let studentsScore = {
 };
 
 // Start coding here
+function avg(AvgScore){
+let sum = 0;
+for(const i in studentsScore){
+  sum += studentsScore[i]
+}
+return sum /Object.keys(studentsScore).length
+}
+let avgSc = avg(studentsScore)
+console.log("Average score is " + avgSc)
+
+let highScore = 0;
+let highName ;
+
+for (const i in studentsScore){
+  if(studentsScore[i] > highScore){
+      highScore = studentsScore[i]
+      highName = i
+  }
+}
+console.log( highName + " has the highest score, which is " + highScore + " points.")
+
+let LowScore = Infinity;
+let LowName ;
+
+for (const i in studentsScore){
+  if(studentsScore[i] < LowScore){
+      LowScore = studentsScore[i]
+      LowName = i
+  }
+}
+console.log(LowName + " has lowest score, which is " + LowScore + " points.")
